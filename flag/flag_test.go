27afde3d67a4cb612057eb7ac136cea3eccdda52
Yufei Ren <yufei.ren@grabtaxi.com>
2019-09-23 11:07:09 +0800

    update

--- flag/flag_test.go
+++ flag/flag_test.go
@@ -1,10 +1,11 @@
 package flag
 
 import "testing"
+import "github.com/stretchr/testify/assert"
 
 func TestFlag(t *testing.T) {
 	flags := int64(0)
-
 	Set(&flags, EnableRun)
+	assert.True(t, true)
 }
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yren/melon-kit
+
+go 1.12
+
+require github.com/stretchr/testify v1.4.0
