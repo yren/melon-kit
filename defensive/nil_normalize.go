748bf74ed88f459649ca0d5b55c36c0f27a9ccce
Yufei Ren <yufei.ren@grabtaxi.com>
2019-09-30 11:41:25 +0800

    update

--- defensive/nil_normalize.go
+++ defensive/nil_normalize.go
@@ -13,19 +13,3 @@ type Dog struct {
 func (d *Dog) Run() {
 	fmt.Printf("%s running...\n", d.Name)
 }
-
-func Normalize() {
-	var ab *Dog
-	var jim Runner
-	jim = ab
-	fmt.Printf("jim equal nil %v", jim == nil)
-
-	v, ok := jim.(*Dog)
-	if ok {
-		if v == nil {
-			jim = nil
-		}
-		return
-	}
-
-}
