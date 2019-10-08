8440397afa378d956833e2abb09d7b5b5dc48e97
Yufei Ren <yufei.ren@grabtaxi.com>
2019-10-08 11:42:46 +0800

    update

--- defensive/nil_normalize.go
+++ defensive/nil_normalize.go
@@ -2,14 +2,17 @@ package defensive
 
 import "fmt"
 
+// Runner ...
 type Runner interface {
 	Run()
 }
 
+// Dog ...
 type Dog struct {
 	Name string
 }
 
+// Run ...
 func (d *Dog) Run() {
 	fmt.Printf("%s running...\n", d.Name)
 }
