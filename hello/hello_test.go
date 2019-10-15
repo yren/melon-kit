e850d726126ef876d70d6ebab86de017b65f7b0e
Yufei Ren <yufei.ren@grabtaxi.com>
2019-10-15 15:11:09 +0800

    update

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,19 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("say hello to people", func(t *testing.T) {
 		got := Hello("jack")
 		want := "hello, jack"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 
-	t.Run("say hello empty", func(t *testing.T) {
+	t.Run("empty hello", func(t *testing.T) {
 		got := Hello("")
 		want := "hello, world"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 }
