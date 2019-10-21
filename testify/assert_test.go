6be14c094c7656d69430f1cbb50bfaebe7baf270
Yufei Ren <yufei.ren@grabtaxi.com>
2019-10-21 16:10:15 +0800

    testify test

---
+++ testify/assert_test.go
@@ -0,0 +1,14 @@
+package testify
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	assert.Equal(t, 123, 123, "they should be equal")
+	assert.NotEqual(t, 123, 456, "they should not equal")
+
+	var obj []int
+	assert.Nil(t, obj)
+}
