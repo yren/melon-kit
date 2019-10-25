980e077ec3a4c76428fff7c50a65e90d802bf39c
Yufei Ren <yufei.ren@grabtaxi.com>
2019-10-25 16:50:49 +0800

    update

---
+++ gowithtest/sum/sum.go
@@ -0,0 +1,13 @@
+package sum
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	return
+}
