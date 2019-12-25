e79d1b8b431a17c2ee05a2d3a9f908e894f589ef
Yufei Ren <ren.yufei@outlook.com>
2019-12-25 23:29:58 +0800

    update

--- integers/addr_test.go
+++ integers/addr_test.go
@@ -1,9 +1,20 @@
 package integers
-import "testing"
-func TestAddr(t *testing.T) {
-    got := Addr(2, 2)
-    expected := 4
-    if got != expected {
-        t.Errorf("got: %d, expected: %d", got, expected)
-    }
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := Add(2, 2)
+	expected := 4
+	if got != expected {
+		t.Errorf("got: %d, expected: %d", got, expected)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
