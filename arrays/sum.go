3793759e18a8edd63ff396d1c793e8854e82dffa
Yufei Ren <ren.yufei@outlook.com>
2019-12-26 23:41:46 +0800

    update

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,18 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, v := range numbers {
 		sum += v
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	numbersLength := len(numbersToSum)
+	sums = make([]int, numbersLength)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,13 +1,40 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+const outputFormat = "expected: %d, but got: %d, given: %v"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	expected := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		expected := 15
 
-	if got != expected {
-		t.Errorf("expected: %d, but got: %d, given: %v", expected, got, numbers)
+		if got != expected {
+			t.Errorf(outputFormat, expected, got, numbers)
+		}
+
+	})
+
+	t.Run("collection of 3 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf(outputFormat, expected, got, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{5, 6})
+	expected := []int{3, 11}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected: %v, but got: %v", expected, got)
 	}
 }
